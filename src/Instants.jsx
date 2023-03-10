1e670798066aa95ad08d82b940b8153cf681d130
Agustín Villalba <villalba.agus.99@gmail.com>
2023-03-10 00:03:06 -0300

    first commit

---
+++ src/Instants.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Instants = () => {
+  return <div>Instants</div>;
+};
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Instants } from "./Instants";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Instants />
+  </React.StrictMode>
+);
